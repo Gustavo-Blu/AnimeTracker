84d851110e74bcf817717c864a7f7345df0089e1
Gustavo Allen <gustavoallen92@gmail.com>
2022-05-06 19:13:59 -0400

    added model for playlists and added associations between all models

--- server/db/index.js
+++ server/db/index.js
@@ -6,6 +6,7 @@ const User = require('./models/User');
 const Artist = require('./models/Artist');
 const Show = require('./models/Show');
 const Song = require('./models/Song');
+const Playlist = require('./models/Playlist');
 
 //associations could go here!
 User.hasMany(Artist);
@@ -17,7 +18,18 @@ Show.belongsTo(User);
 User.hasMany(Song);
 Song.belongsTo(User);
 
+User.hasMany(Playlist);
+Playlist.belongsTo(User);
+
 //----------------------------
+Show.belongsToMany(Song, { through: 'soundtrack' });
+Song.belongsToMany(Show, { through: 'soundtrack' });
+
+Artist.belongsToMany(Song, { through: 'disc' });
+Song.belongsToMany(Artist, { through: 'disc' });
+
+Playlist.belongsToMany(Song, { through: 'playlistSong' });
+Song.belongsToMany(Playlist, { through: 'playlistSong' });
 
 module.exports = {
   db,
---
+++ server/db/models/Playlist.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Playlist = db.define('playlist', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'https://yt3.ggpht.com/8cd9vG0u0Mxe53OFiFIpfR8SSKS5FPSAHgMyJN3szWc08bYMHHctXc6DGGDGdy7j59Ia6dnP7WE=s900-c-k-c0x00ffffff-no-rj',
+  },
+});
+
+module.exports = Playlist;
